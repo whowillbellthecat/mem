597fcd512a3a0f548729dabc8767d8403826e642
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-07-19 20:21:35 -0500

    change default logfile name

--- config.pl
+++ config.pl
@@ -1,5 +1,5 @@
 home(X,Y):-environ('HOME',H),atom_join([H,X],'/',Y).
 pdfviewer(mupdf).
-logfile(X):-memdir(M),atom_join([M,'dat.pl'],'/',X).
+logfile(X):-memdir(M),atom_join([M,'history.pl'],'/',X).
 carddir(X):-home('.cards',X).
 memdir(X):-home('.mem',X).
